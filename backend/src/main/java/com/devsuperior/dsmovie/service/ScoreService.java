7904cd697dde87b332565577bad0a5cd3c031794
thaysgama <thays.s.gama@gmail.com>
2022-01-13 21:53:28 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsmovie/service/ScoreService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/service/ScoreService.java
@@ -27,31 +27,30 @@ public class ScoreService {
     @Transactional
     public MovieDTO saveScore(ScoreDTO scoreDTO){
         User user = userRepository.findByEmail(scoreDTO.getEmail());
-        if(user==null){
+        if(user==null) {
             user = new User();
             user.setEmail(scoreDTO.getEmail());
             user = userRepository.saveAndFlush(user);
-
-            Movie movie = movieRepository.findById(scoreDTO.getMovieId()).get();
-
-            Score score = new Score();
-            score.setMovie(movie);
-            score.setUser(user);
-            score.setValue(scoreDTO.getScore());
-
-            score = scoreRepository.saveAndFlush(score);
-
-            double sum = 0.0;
-            for (Score s : movie.getScores()){
-                sum += s.getValue();
-            }
-            double avg = sum / movie.getScores().size();
-            movie.setScore(avg);
-            movie.setCount(movie.getScores().size());
-
-            movie = movieRepository.save(movie);
-            return new MovieDTO(movie);
         }
-        return null;
+        Movie movie = movieRepository.findById(scoreDTO.getMovieId()).get();
+
+        Score score = new Score();
+        score.setMovie(movie);
+        score.setUser(user);
+        score.setValue(scoreDTO.getScore());
+
+        score = scoreRepository.saveAndFlush(score);
+
+        double sum = 0.0;
+        for (Score s : movie.getScores()) {
+            sum += s.getValue();
+        }
+        double avg = sum / movie.getScores().size();
+        movie.setScore(avg);
+        movie.setCount(movie.getScores().size());
+
+        movie = movieRepository.save(movie);
+        return new MovieDTO(movie);
+
     }
 }
